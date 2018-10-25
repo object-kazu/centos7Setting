61408271a7cf2b1b1f0bc60cd29cd2117a776a3d
kazuyuki shimizu <kazuyukishimizu@me.com>
2018-10-25 14:15:10 +0900

    install rails

--- run.sh
+++ run.sh
@@ -4,3 +4,17 @@ sudo yum install -y emacs
 
 #prep for installing rbenv
 sudo yum install -y gcc-c++ glibc-headers openssl-devel readline libyaml-devel readline-devel zlib zlib-devel
+sudo yum install -y gcc sqlite-devel libicu-devel cmake
+
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(rbenv init -)"' >> ~/.bash_profile
+source ~/.bash_profile
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+
+rbenv install -l
+rbenv install 2.4.0
+rbenv global 2.4.0
+ruby --version
+
+gem install rails
