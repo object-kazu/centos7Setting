8eeeeef725f35bf746368980ec9656f83403d1ad
kazuyuki shimizu <kazuyukishimizu@me.com>
2018-10-25 14:02:04 +0900

    install emacs

--- run.sh
+++ run.sh
@@ -1,17 +1,4 @@
-# gitを使ってアプリケーション設定用のスクリプトをダウンロード
-#git clone https://github.com/dotinstallres/centos6.git
-
-# centos6フォルダができるのでそちらに移動
-# cd centos6
-
-# スクリプトを実行（時間かかります）
-#./run.sh
-
-
 #!/bin/bash
 
-sudo yum -y update
-
-# スクリプトを入手するためのgitをインストール
-sudo yum -y install git
+sudo yum install -y emacs
 
